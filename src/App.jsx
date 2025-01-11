b44e031d0af39fbb1f54fa11d2ae6f77a24adc92
Teeraphat Jirapasnitilet <110732968+teeraphat0806@users.noreply.github.com>
2025-01-11 10:58:55 +0700

    Your submodule changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
-import { useState } from 'react'
-import { createRoot } from "react-dom/client"
-import { BrowserRouter, Routes, Router} from 'react-router-dom';
-
-
-import Home from './components/Home'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
-
-
   return (
-  <Router>
-    <Routes>
-     <Router path="/home" element={<Home />}/>
-    </Routes>
-  </Router>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,49 +1,38 @@
-import React from "react";
+import React from 'react';
+import Navbar from '../components/navbar';
+import Card from '../components/cardprofile';
+import CardDetil from '../components/carddetails';
+import ProfilDeil from '../components/profiledetail';
+import Social from '../components/social_media';
 
-import Navbar from '../components/navbar'
-import Card from '../components/cardprofile'
-import CardDetil from '../components/carddetails'
-import ProfilDeil from '../components/profiledetail'
-import Social from '../components/social_media'
-
-
-
-const Home =()=>
-{
-   
+const Home = () => {
   return (
-    <div className="">
-     
     <div>
-     
+      <div>
+        <Navbar />
+        <br />
+      </div>
+
+      <div>
+        <Card />
+        <br />
+      </div>
+
+      <div className="container mt-4 mb-4 px-5 py-2">
+        <CardDetil />
+      </div>
+
+      <div className="container mt-4 mb-4 px-5 py-2">
+        <ProfilDeil />
+      </div>
+
+      <hr className="featurette-divider" />
+
+      <div className="container px-5 py-5">
+        <Social />
+      </div>
     </div>
-    
-    <div>
-      <Navbar />
-      <br />
-    </div>  
-    
-    <div>
-      <Card/>
-      <br />
-    </div> 
-  
-    <div class="container  mt-4 mb-4 px-5 py-2">
-      <CardDetil/>
-    </div>
-  
-    <div class="container  mt-4 mb-4 px-5 py-2">
-        <ProfilDeil/>
-    </div>
-  
-    <hr class="featurette-divider"></hr>
-    
-    <div class='container px -5 py-5'>
-    <Social/>
-    </div>
-     
-     
-    </div>
-  )  
-}
-export default Home
+  );
+};
+
+export default Home;
