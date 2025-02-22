f753d9863b9f8b151ac881b807c1ef6f3a982fa2
Teeraphat Jirapasnitilet <110732968+teeraphat0806@users.noreply.github.com>
2025-02-22 12:34:30 +0700

    ver 2.9

--- src/data/webdata.jsx
+++ src/data/webdata.jsx
@@ -3,7 +3,7 @@ const WebData = [
       imgSrc:
         "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       iconSrc:
-        "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbAmafdPNr9fd0KC0Z98WYEC7Wl1wYlPVf-A&s",
       title: "Numerical Method",
       description: "React App อธิบายฟังก์ชันการใช้งาน แต่ละทฤษฏีในวิชา numerical method",
       date: "มีนาคม 2564",
@@ -13,7 +13,7 @@ const WebData = [
       imgSrc:
         "https://lh3.googleusercontent.com/d/1PmWW_ziqZAID2ur_7kEbtSIObQm9-y41=w1000",
       iconSrc:
-        "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbAmafdPNr9fd0KC0Z98WYEC7Wl1wYlPVf-A&s",
       title: "Reant Game",
       description: "Website เช่าเกมส์ Frontend React Backend prisma Postgues",
       date: "กรกฎาคม 2565",
@@ -22,7 +22,7 @@ const WebData = [
     {
       imgSrc: "https://lh3.googleusercontent.com/d/1g38KLUXk1tY1dW9sCuNgmtnRGi17WG-5",
       iconSrc:
-        "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbAmafdPNr9fd0KC0Z98WYEC7Wl1wYlPVf-A&s",
       title: "Website-renting-game",
       description: "Website เช่าเกมส์ Frontend React Backend PHP MYSQL",
       date: "ตุลาคม 2566",
